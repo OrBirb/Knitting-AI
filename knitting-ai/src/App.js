9c89507930fd58cfd25c705d03f8efb808e5e0a5
OrBirb <orbirb.llc@gmail.com>
2025-02-26 16:06:11 -0800

    Updated homepage message

--- knitting-ai/src/App.js
+++ knitting-ai/src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Knitting AI – Your Smart Knitting Assistant!
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
